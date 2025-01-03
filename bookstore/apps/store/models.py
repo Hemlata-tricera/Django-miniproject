c1068a365a9d0db83f993602e5615fd535db3c10
Hemlata <hemlata.raghuvanshi@tricera.tech>
2025-01-03 13:32:31 +0530

    Add search button and fetch all books on homepage

--- bookstore/apps/store/models.py
+++ bookstore/apps/store/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+
+    name = models.CharField(max_length=100, help_text="Enter category Science Fiction, Horror, Thriller/Suspense, Historical Fiction, and Contemporary Fiction. etc")
+
+    description = models.TextField()
+
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    genre = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
--- bookstore/apps/store/views.py
+++ bookstore/apps/store/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
+
 
 # Create your views here.
+def homepage(request):
+    query = request.GET.get('query', '')  # Get search query from URL parameters
+    books = Book.objects.all()  # Simple search
+
+    if query:
+        # Filter books by title, author, or price
+        books = books.filter(
+            title__icontains=query) | books.filter(
+            author__icontains=query) | books.filter(
+            price__icontains=query)
+
+
+    return render(request, 'homepage.html', {'query': query, 'books': books})
