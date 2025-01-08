c38b61f72b2f9714b6b7eb8bc54198ada94758bf
Hemlata <hemlata.raghuvanshi@tricera.tech>
2025-01-08 15:44:38 +0530

    Add basic authentication

--- bookstore/apps/store/urls.py
+++ bookstore/apps/store/urls.py
@@ -14,6 +14,9 @@ urlpatterns = [
     path('update/<int:item_id>/', views.update_cart_item, name='update_cart_item'),
     path('cart/', views.cart_detail, name='cart_detail'),
     path('remove/<int:book_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('login/', login_page, name='login_page'),    # Login page
+    path('logout/', logout_page, name='logout_page'),    # Login page
+    path('register/', register_page, name='register'),  # Registration page
 
 
 ]
--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login_page'  # If 'login_page' is the name of your login path
